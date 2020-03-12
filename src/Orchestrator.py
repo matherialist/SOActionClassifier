7777a4e257202fe4a2a80640bd3f15b99f611adc
matherialist <lagrange@phystech.edu>
2020-03-12 15:54:06 +0300

    .idea ignore

--- src/Orchestrator.py
+++
@@ -1,9 +0,0 @@
-from src.ActionClassifer import ActionClassifier
-from src.SpeechToText import SpeechToText
-from src.TextToSpeech import TextToSpeech
-
-
-class Orchestrator:
-
-    def __init__(self):
-        self.action_classifier = ActionClassifier("files")
